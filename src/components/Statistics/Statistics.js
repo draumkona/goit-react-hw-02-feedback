268808186f52e313a87a9e49144ec633b2b096e3
Gosia Stepaniak <g.a.stepaniak@gmail.com>
2023-05-26 20:16:46 +0200

    up

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 export const Statistics = props => {
   const { good, neutral, bad, total, positive } = props;
